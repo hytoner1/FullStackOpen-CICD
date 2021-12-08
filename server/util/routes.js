07a54a4f3ff44694670adc605f50df87619a48ea
Roni Hytonen <roni.hytonen93@gmail.com>
2021-12-08 18:45:51 +0200

    Add workflows

--- server/util/routes.js
+++ server/util/routes.js
@@ -3,6 +3,10 @@ const anecdotes = require('@controllers/anecdotesController')
 
 const router = Router()
 
+router.get('/health', (req, res) => {
+  res.send('ok')
+})
+
 router.get('/anecdotes', anecdotes.getAll)
 router.post('/anecdotes', anecdotes.create)
 router.delete('/anecdotes/:id', anecdotes.destroy)
